83fd23d7e6b899e95dd347ab1d848afb9691cc56
Juancho Fonseca <fonsecaesc@gmail.com>
2017-06-21 11:09:14 +0200

    Portmap config

--- xen-nfs.sh
+++ xen-nfs.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 ## Enable NFS XenServer
+#client and Server:
+
 modprobe nfsd
+#/etc/hosts.allow
+ALL:ALL
+
+#/etc/sysconfig/portmap
+#PMAP_ARGS=-l
+PMAP_ARGS=""
+
+service iptables stop
 service portmap start
 service nfs start
 chkconfig portmap on
 chkconfig nfs on
 
+
 #Autostart on Boot!
 echo '#!/bin/sh' > /etc/sysconfig/modules/nfsd.modules
 echo 'exec /sbin/modprobe nfsd > /dev/null 2>&1' >> /etc/sysconfig/modules/nfsd.modules
 chmod +x /etc/sysconfig/modules/nfsd.modules
+
+service portmap restart
+service nfs restart
